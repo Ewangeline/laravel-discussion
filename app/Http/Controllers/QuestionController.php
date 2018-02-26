2daff15379af0a57aa308178ca7f7b57b1272a50
Unknown <zerayah@gmail.com>
2018-02-26 11:35:04 +0100

    navbar, index content

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class QuestionController extends Controller
 {
     public function index(){
-        return 'This is the list of questions';
+        $view= view('questions.index');
+        return $view;
+
     }
 
     public function show(){
