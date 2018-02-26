2793466239e1ff020b69fffc67ecee0b58135877
Unknown <zerayah@gmail.com>
2018-02-26 14:56:27 +0100

    creating tables in mysql (migrate)

---
+++ database/migrations/2018_02_26_132822_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');       //adds id, AI, PK
+            $table->integer('user_id');     //adds user_id (int(11))
+            $table->integer('questions_id');//adds questions_id (int(11))
+            $table->text('text');           //adds (TEXT)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
