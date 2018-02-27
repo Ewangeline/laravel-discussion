598ba3c9b07c268bcf086eaaf46b9171f6ac20d2
Unknown <zerayah@gmail.com>
2018-02-27 14:17:33 +0100

    prep for forms

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,6 +10,8 @@ class QuestionController extends Controller
     public function index(){
         $questions = DB::table('questions')->get();
 
+        //$questions = \App\question::notEmpty();
+
         $view= view('questions.index');
         $view->questions = $questions;
         return $view;
@@ -17,9 +19,16 @@ class QuestionController extends Controller
      }
 
     public function show($id){
-        $question = DB::table('questions')->find($id);
+        // $question = DB::table('questions')->find($id);
+        // $question = DB::table('questions')->where('id', $id)->first();
+        // $question = DB::table('questions')->where('id', '=', $id)->first();
+        $question = \App\Questions::find($id);
+
+        //create a new view object
         $view = view('questions.show');
         $view->question = $question;
+
+        //return view
         return $view;
     }
 }
